469be07477c04c98abcf357ead834b858b07787a
Brukie G <brukieg@gmail.com>
2018-06-28 00:12:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (var lowercase = 'hello';
-  lowercase.toLowerCase() === lowercase)
-  return('I can\'t hear you!')
+  lowercase.toLowerCase() === lowercase);
+  {return('I can\'t hear you!')}
 
   var uppercase = 'HELLO'
   uppercase.toUpperCase() === uppercase
