3a325cfe4a8a1191d82dd6d1a37aaaca6e9ea287
Brukie G <brukieg@gmail.com>
2018-06-28 00:05:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log('HELLO')
+}
+function logWhisper(string) {
+  console.log('hello')
+}
+function sayHiToGrandma(string) {
+  var lowercase = 'hello';
+  lowercase.toLowerCase() === lowercase
+  return('I can\'t hear you!')
+
+  var uppercase = 'HELLO'
+  uppercase.toUpperCase() === uppercase
+  return('YES INDEED!')
+
+  console.log('I love you, Grandma.')
+  return('I love you, too.')
+}
